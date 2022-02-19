7de2c060a86080fa8c97bc81449b74e891be223a
angular403 <angularsprings403@gmail.com>
2022-02-19 07:32:40 +0700

    belajar model

--- lib/belajar_api/latihan_future_builder.dart
+++ lib/belajar_api/latihan_future_builder.dart
@@ -2,55 +2,48 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:latihan_week3/models/user.dart';
 
 // ignore: must_be_immutable
 class LatihanFutureBuilder extends StatelessWidget {
   // const LatihanFutureBuilder({Key? key}) : super(key: key);
 
-  List<Map<String, dynamic>> allUser = [];
+  List<UserModel> allUser = [];
 
   Future getAllUser() async {
-    try {
-      var response = await http.get(Uri.parse("https://reqres.in/api/users"));
-      List data = (json.decode(response.body) as Map<String, dynamic>)["data"];
-      // data.forEach((e) {
-      //   if (e % 2 == 1) allUser.add(e);
-      // });
-
-      // data.removeWhere((e) => allUser.contains(e));
-      data.forEach((element) {
-        allUser.add(element);
-      });
-      print(allUser);
-    } catch (e) {
-      print("Error }");
-      print(e);
-    }
+    var response = await http.get(Uri.parse("https://reqres.in/api/users"));
+    List data = (jsonDecode(response.body) as Map<String, dynamic>)['data'];
+    data.forEach((element) {
+      allUser.add(
+        UserModel.fromJson(element),
+      );
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Future Builder"),
+        title: Text("Latihan Future Builder"),
       ),
       body: FutureBuilder(
           future: getAllUser(),
           builder: (context, snapshot) {
             if (snapshot.connectionState == ConnectionState.waiting) {
               return Center(
-                child: Text("Loading ...."),
+                child: CircularProgressIndicator(
+                  color: Colors.green,
+                ),
               );
             } else {
               return ListView.builder(
                 itemCount: allUser.length,
                 itemBuilder: (context, index) => ListTile(
                   leading: CircleAvatar(
-                    backgroundImage: NetworkImage(allUser[index]['avatar']),
+                    backgroundImage: NetworkImage("${allUser[index].avatar}"),
                   ),
-                  title: Text(
-                      "${allUser[index]['first_name']} ${allUser[index]['last_name']}"),
-                  subtitle: Text("${allUser[index]['email']}"),
+                  title: Text("${allUser[index].firstName}} ${allUser[index].lastName}"),
+                  subtitle: Text("${allUser[index].email}"),
                 ),
               );
             }
